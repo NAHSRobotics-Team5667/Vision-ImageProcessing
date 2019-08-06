0c4d2cba218014158ecd292b6ff2abcbce6cb8c0
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2019-08-06 16:26:56 -0400

    dcocumented code and updated readme to include code snippets

--- README.md
+++ README.md
@@ -3,26 +3,89 @@
 A simple vision tracking example using a sample image from the internet and OpenCV.
 The vision tracking code can be found [here](src/test/java/frc/Vision.java).
 
+This is a sample frame of what a camera might see:
+
+![image of rocket](src/test/java/frc/assets/RocketTape.jpeg)
+
 We start the image processing by converting the image into gray scale.
+
+```java
+Imgproc.cvtColor(defaultFrame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+```
 
 ![gray scale image](src/test/java/frc/output/01_grayScale.jpeg)
 
 We then blur the image so that we can smooth out our image.
 
+```java
+Imgproc.blur(grayFrame, blurredGrayFrame, new Size(3, 3));
+```
+
 ![blur](src/test/java/frc/output/02_blurredGray.jpeg)
 
 Next we use a technique called thresholding to "filter" any values we don't want.
 
+```java
+Imgproc.threshold(blurredGrayFrame, threshFrame, 240, 255, Imgproc.THRESH_BINARY);
+```
+
 ![thresholding](src/test/java/frc/output/03_threshold.jpeg)
 
-We then use an algorithm called canny, which finds the edges of each item left after the threshold and returns the contours of each object.
+We then use an algorithm called canny, which allows us to run another algorithm that finds the contours.
+
+```java
+Imgproc.Canny(threshFrame, cannyFrame, 240, 255);
+Imgproc.findContours(cannyFrame, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+```
 
 ![canny](src/test/java/frc/output/04_canny.jpeg)
 
-We can now draw bounding boxes on each object we were able to find.
+We can now draw bounding boxes on each object we were able to find using the contours.
+
+```java
+// Iterate through each contour and approximate the bounding boxes
+for (int i = 0; i < contours.size(); i++) {
+    contoursPoly[i] = new MatOfPoint2f();
+    Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
+    boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
+    // Add the filtering info after the bounding box has been created
+    tl_Corners[i] = boundRect[i].tl();
+    areas[i] = boundRect[i].area();
+}
+// Store each polygon we were able to create after approximating the bounding
+// box curves
+List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursPoly.length);
+for (MatOfPoint2f poly : contoursPoly) {
+    contoursPolyList.add(new MatOfPoint(poly.toArray()));
+}
+
+```
 
 ![bounding boxes](src/test/java/frc/output/05_rectangles.jpeg)
 
 Finally, we perform some checks on each object to filter the noise and leave only the actual targets.
+
+```java
+// Filter algorithm
+for (int i = 0; i < contours.size(); i++) {
+    // If we have 2 potential targets then stop running the check
+    if (potentialTargets.size() == 2)
+        break;
+    // Loop through every other bounding box and perform checks relative to itself
+    for (int j = 0; j < contours.size(); j++) {
+        if (j != i && Math.abs(areas[j] - boundRect[i].area()) <= areaTolerance
+                && boundRect[i].width < boundRect[i].height && boundRect[i].area() > MIN_AREA) {
+            potentialTargets.add(boundRect[i]);
+            Imgproc.drawContours(frame, contoursPolyList, i, green);
+            break;
+        }
+    }
+}
+// Draw the bounding box encapsulating the targets
+Imgproc.rectangle(frame, potentialTargets.get(1).tl(), potentialTargets.get(0).br(), white, 1);
+// Return the processed frame
+return frame;
+}
+```
 
 ![targets](src/test/java/frc/output/06_targets.jpeg)
--- src/test/java/frc/Vision.java
+++ src/test/java/frc/Vision.java
@@ -30,12 +30,13 @@ import org.opencv.imgproc.Imgproc;
 public class Vision {
     @Test
     public void visionTest() {
-
+        // Pre-load
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
+        // Get the default image
         String path = "./src/test/java/frc/assets/RocketTape.jpeg";
         Mat defaultFrame = Imgcodecs.imread(path);
-
+        // Mats (can be thought of as digital frames, contains info about each
+        // individual pixel in a frame)
         Mat grayFrame = new Mat();
         Mat blurredGrayFrame = new Mat();
         Mat threshFrame = new Mat();
@@ -43,9 +44,9 @@ public class Vision {
         Mat hierarchy = new Mat();
         Mat rectFrame = new Mat();
         Mat targetsFrame = new Mat();
-
+        // Our contours that we will be using later to find the bounding boxes
         List<MatOfPoint> contours = new ArrayList<>();
-
+        // Image processing
         Imgproc.cvtColor(defaultFrame, grayFrame, Imgproc.COLOR_BGR2GRAY);
         Imgproc.blur(grayFrame, blurredGrayFrame, new Size(3, 3));
         Imgproc.threshold(blurredGrayFrame, threshFrame, 240, 255, Imgproc.THRESH_BINARY);
@@ -55,6 +56,7 @@ public class Vision {
         rectFrame = drawRectAroundContours(cannyFrame, contours);
         targetsFrame = getTargets(cannyFrame, contours);
 
+        // Visual output when running the test
         HighGui.imshow("Initial", defaultFrame);
         HighGui.imshow("Gray Scale", grayFrame);
         HighGui.imshow("Blurred Gray Scale", blurredGrayFrame);
@@ -63,85 +65,113 @@ public class Vision {
         HighGui.imshow("Rectangles", rectFrame);
         HighGui.imshow("Targets", targetsFrame);
 
+        // Save the visual output into files in the output folder
         Imgcodecs.imwrite("./src/test/java/frc/output/01_grayScale.jpeg", grayFrame);
         Imgcodecs.imwrite("./src/test/java/frc/output/02_blurredGray.jpeg", blurredGrayFrame);
         Imgcodecs.imwrite("./src/test/java/frc/output/03_threshold.jpeg", threshFrame);
         Imgcodecs.imwrite("./src/test/java/frc/output/04_canny.jpeg", cannyFrame);
         Imgcodecs.imwrite("./src/test/java/frc/output/05_rectangles.jpeg", rectFrame);
         Imgcodecs.imwrite("./src/test/java/frc/output/06_targets.jpeg", targetsFrame);
-
+        // Don't finish the test so that we can see the visual output
         HighGui.waitKey();
     }
 
+    /**
+     * Draw bounding boxes based on the contours found
+     * 
+     * @param cannyOutput - The output Mat produced after running Canny()
+     * @param contours    - The output contours produced after running Canny()
+     * @return A new Mat with the contours and bounding boxes drawn
+     */
     public Mat drawRectAroundContours(Mat cannyOutput, List<MatOfPoint> contours) {
+        // Our output frame currently set as a black frame
         Mat frame = Mat.zeros(cannyOutput.size(), CvType.CV_8UC3);
 
+        // The contours that we find
         MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
+        // The bounding boxes we make
         Rect[] boundRect = new Rect[contours.size()];
-        Point[] tl_Corners = new Point[contours.size()];
 
+        // Iterate through each contour and approximate the bounding boxes
         for (int i = 0; i < contours.size(); i++) {
             contoursPoly[i] = new MatOfPoint2f();
             Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
             boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
         }
 
+        // Store each polygon we were able to create after approximating the bounding
+        // box curves
         List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursPoly.length);
         for (MatOfPoint2f poly : contoursPoly) {
             contoursPolyList.add(new MatOfPoint(poly.toArray()));
         }
 
+        // Draw the bounding boxes
         for (int i = 0; i < contours.size(); i++) {
-            tl_Corners[i] = boundRect[i].tl();
-        }
-
-        for (int i = 0; i < contours.size(); i++) {
+            // Color scalars
             Scalar green = new Scalar(0, 255, 0);
             Scalar white = new Scalar(255, 255, 255);
+            // Draw functions
             Imgproc.drawContours(frame, contoursPolyList, i, green);
             Imgproc.rectangle(frame, boundRect[i].tl(), boundRect[i].br(), white, 1);
 
         }
-
+        // Return the modified frame
         return frame;
     }
 
+    /**
+     * Draw bounding boxes after filtering the contours found to show the targets
+     * only
+     * 
+     * @param cannyOutput - The output Mat produced after running Canny()
+     * @param contours    - The output contours produced after running Canny()
+     * @return A new Mat with the contours and bounding boxes drawn on only the
+     *         targets
+     */
     public Mat getTargets(Mat cannyOutput, List<MatOfPoint> contours) {
+        // The margin of area allowed when filtering the targets
         double areaTolerance = 3;
+        // The minimum area for the object to be considered
         double MIN_AREA = 10;
-
+        // Color scalars
         Scalar green = new Scalar(0, 255, 0);
         Scalar white = new Scalar(255, 255, 255);
-
+        // Empty frame
         Mat frame = Mat.zeros(cannyOutput.size(), CvType.CV_8UC3);
-
+        // List of the polygons found
         MatOfPoint2f[] contoursPoly = new MatOfPoint2f[contours.size()];
+        // List of the bounding boxes found
         Rect[] boundRect = new Rect[contours.size()];
+        // List of the top left corners of the bounding boxes for filtering
         Point[] tl_Corners = new Point[contours.size()];
+        // List of the areas of each bounding box for filtering
         double[] areas = new double[contours.size()];
-
+        // Array list of the potential targets found
         List<Rect> potentialTargets = new ArrayList<>(2);
 
+        // Iterate through each contour and approximate the bounding boxes
         for (int i = 0; i < contours.size(); i++) {
             contoursPoly[i] = new MatOfPoint2f();
             Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly[i], 3, true);
             boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
+            // Add the filtering info after the bounding box has been created
+            tl_Corners[i] = boundRect[i].tl();
+            areas[i] = boundRect[i].area();
         }
-
+        // Store each polygon we were able to create after approximating the bounding
+        // box curves
         List<MatOfPoint> contoursPolyList = new ArrayList<>(contoursPoly.length);
         for (MatOfPoint2f poly : contoursPoly) {
             contoursPolyList.add(new MatOfPoint(poly.toArray()));
         }
 
+        // Filter algorithm
         for (int i = 0; i < contours.size(); i++) {
-            tl_Corners[i] = boundRect[i].tl();
-            areas[i] = boundRect[i].area();
-
-        }
-
-        for (int i = 0; i < contours.size(); i++) {
+            // If we have 2 potential targets then stop running the check
             if (potentialTargets.size() == 2)
                 break;
+            // Loop through every other bounding box and perform checks relative to itself
             for (int j = 0; j < contours.size(); j++) {
                 if (j != i && Math.abs(areas[j] - boundRect[i].area()) <= areaTolerance
                         && boundRect[i].width < boundRect[i].height && boundRect[i].area() > MIN_AREA) {
@@ -151,9 +181,9 @@ public class Vision {
                 }
             }
         }
-
+        // Draw the bounding box encapsulating the targets
         Imgproc.rectangle(frame, potentialTargets.get(1).tl(), potentialTargets.get(0).br(), white, 1);
-
+        // Return the processed frame
         return frame;
     }
 
